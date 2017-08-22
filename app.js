b17b0bd85940111407b67f4b421164ef5b014f4c
Sarah Meyer <Smeyer@Sarahs-MacBook-Pro.local>
2017-08-22 15:41:45 -0600

    fading divs

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-$(window).on("load",function() {
-  $(window).scroll(function() {
-    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-    $(".fade").each(function() {
-      /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
-
-      /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-      } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
-      }
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.fade').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                $(this).addClass('show');
+            }
+            if( bottom_of_window < bottom_of_object ){
+                $(this).removeClass('show');
+            }
+        });
     });
-  }).scroll(); //invoke scroll-handler on page-load
 });
